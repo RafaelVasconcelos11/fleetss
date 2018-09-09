24fd8fa594f6c155bd85b143a355c2107afc6b75
Rafael Antônio Vasconcelos Japyassu <rafael11dev@gmail.com>
2018-09-09 19:44:32 -0300

    Início do projeto

---
+++ src/main/java/com/attr/fleetss/FleetSsApplication.java
@@ -0,0 +1,12 @@
+package com.attr.fleetss;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FleetSsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FleetSsApplication.class, args);
+    }
+}
---
+++ src/main/java/com/attr/fleetss/model/Marca.java
@@ -0,0 +1,32 @@
+package com.attr.fleetss.model;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "tb_marca")
+public class Marca {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id_marca")
+    private Long id;
+
+    @Column(name = "nm_marca")
+    private String nome;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+}
